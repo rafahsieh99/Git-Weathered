ffc7108e3516af43602afa0e0fc3a8f1e76fc197
rafahsieh99 <rafahsieh99@gmail.com>
2024-09-25 22:45:16 -0400

    Funciones relacionadas con la API

--- clima.py
+++ clima.py
@@ -1,8 +1,9 @@
 import requests
+from config import API_KEY
 
 
 def get_weather_data(location):
-    api_key = ""  # Reemplaza con tu propia API key de OPENWEATHERMAP
+    api_key = API_KEY  # Reemplaza con tu propia API key de OPENWEATHERMAP
     base_url = "http://api.openweathermap.org/data/2.5/weather"
     params = {"q": location, "appid": api_key, "units": "metric"}
     response = requests.get(base_url, params=params)
