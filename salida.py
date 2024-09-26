f79c00354da167108e6f72fd07c39d713cc80f78
rafahsieh99 <rafahsieh99@gmail.com>
2024-09-25 22:07:21 -0400

    Funciones para mostrar salida

--- salida.py
+++ salida.py
@@ -1,22 +1,22 @@
 import json
 
+
 def print_weather(data):
     if data:
         temp = round(data['main']['temp'], 2)
         feels_like = round(data['main']['feels_like'], 2)
         location = data['name']
         description = data['weather'][0]['description']
-        
         print(f"Clima en {location}:")
         print(f"Temperatura: {temp}°C")
         print(f"Sensación térmica: {feels_like}°C")
         print(f"Condiciones: {description}")
         print(f"Humedad: {data['main']['humidity']}%")
 
+
 def print_as_json(data):
     temp = round(data['main']['temp'], 2)
     feels_like = round(data['main']['feels_like'], 2)
-    
     data_to_print = {
         'ubicacion': data['name'],
         'temperatura': temp,
@@ -26,11 +26,12 @@ def print_as_json(data):
     }
     print(json.dumps(data_to_print, indent=2, ensure_ascii=False))
 
+
 def print_as_csv(data):
-    temp = round(data['main']['temp'])
-    feels_like = round(data['main']['feels_like'])
+    temp = round(data['main']['temp'], 2)
+    feels_like = round(data['main']['feels_like'], 2)
     location = data['name']
     description = data['weather'][0]['description']
-    
-    print(f"ubicacion,temperatura,sensacion_termica,descripcion,humedad")
-    print(f"{location},{temp},{feels_like},{description},{data['main']['humidity']}")
+    print("ubicacion,temperatura,sensacion_termica,descripcion,humedad")
+    print(f"{location},{temp},{feels_like},"
+          f"{description},{data['main']['humidity']}")
