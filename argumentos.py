d0736fa1f10e5bc8297067121f2377ec7a5c224a
rafahsieh99 <rafahsieh99@gmail.com>
2024-09-26 16:39:21 -0400

    Funcion para los argumentos

---
+++ argumentos.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Consulta el clima de una ubicación.")
+    parser.add_argument(
+        "location", type=str,
+        help="Nombre de la ciudad y país (Ej: Asuncion-PY)")
+    parser.add_argument(
+        "-format", type=str,
+        choices=["json", "csv", "text"],
+        default="text", help="Formato de salida")
+    return parser.parse_args()
