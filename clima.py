79f17804a87a96df0172abb00a964fe7df954cf8
rafahsieh99 <rafahsieh99@gmail.com>
2024-09-25 21:55:37 -0400

    Correccion

--- clima.py
+++ clima.py
@@ -1,7 +1,8 @@
 import requests
 
+
 def get_weather_data(location):
-    api_key = "489cf306c218f90bc09f9539bd195c81"  # Reemplaza con tu propia API key
+    api_key = ""  # Reemplaza con tu propia API key
     base_url = "http://api.openweathermap.org/data/2.5/weather"
     params = {"q": location, "appid": api_key, "units": "metric"}
     response = requests.get(base_url, params=params)
